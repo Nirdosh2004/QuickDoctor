19dbbb2239656fafbff3857a797234887ea8d87c
nirdosh2004 <hrishabhkushwaha73@gmail.com>
2025-03-19 12:19:05 +0530

    7:53:58 Not showing data in MongoDb atlas

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
 import { AdminContext } from '../../context/AdminContext'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 const AddDoctor = () => {
 
@@ -20,6 +22,45 @@ const AddDoctor = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
+
+    try {
+      if(!docImg){
+        return toast.error('Image not selected!')
+      }
+
+      const formData = new FormData()
+
+      formData.append('image',docImg)
+      formData.append('name',name)
+      formData.append('email',email)
+      formData.append('password',password)
+      formData.append('experience',experience)
+      formData.append('fees',Number(fees))
+      formData.append('about',about)
+      formData.append('speciality',speciality)
+      formData.append('degree',degree)
+      formData.append('address',JSON.stringify({line1:address1,line2:address2}))
+
+      // console.log formData
+      formData.forEach((value,key)=>{
+        console.log(`${key} : ${value}`);
+        
+      })
+
+      const {data} = await axios.post(backendUrl + '/api/admin/add-doctor',formData,{headers:{aToken}})
+      
+      if(data.success){
+        toast.success(data.message)
+      }
+      else{
+        toast.error(data.message)
+      }
+
+
+    } catch (error) {
+      
+    }
+
   }
 
 
