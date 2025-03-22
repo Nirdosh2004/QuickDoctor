21b5bd7a906b2ae58318b089ca420aca90c1211d
nirdosh2004 <hrishabhkushwaha73@gmail.com>
2025-03-22 14:29:56 +0530

    11:10:27

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -56,7 +56,7 @@ const Appointment = () => {
         let month = currentDate.getMonth() + 1
         let year = currentDate.getFullYear()
 
-        const slotDate = day + "_" + month + "_" + year
+        const slotDate = day + "/" + month + "/" + year
         const slotTime = formattedTime
 
         const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true
@@ -92,7 +92,7 @@ const Appointment = () => {
       let month = date.getMonth() + 1
       let year = date.getFullYear()
 
-      const slotDate = day + "_" + month + "_" + year
+      const slotDate = day + "/" + month + "/" + year
 
       const { data } = await axios.post(backendUrl + '/api/user/book-appointment', { docId, slotDate, slotTime }, { headers: { token } })
       if (data.success) {
