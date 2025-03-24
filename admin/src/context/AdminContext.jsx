ec6d2b3eeec68ef1ed3a693ce25bac1a4b1f9e25
nirdosh2004 <hrishabhkushwaha73@gmail.com>
2025-03-24 08:54:32 +0530

    13:06:46

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -9,6 +9,7 @@ const AdminContextProvider = (props) => {
     const [aToken,setAToken] = useState(localStorage.getItem('aToken')?localStorage.getItem('aToken'):'')
     const [doctors,setDoctors] = useState([])
     const [appointments, setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const backendUrl = import.meta.env.VITE_BACKEND_URL
 
@@ -86,6 +87,24 @@ const AdminContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+
+            const {data} = await axios.get(backendUrl + '/api/admin/dashboard', {headers:{aToken}})
+
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+            
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         aToken,
         setAToken,
@@ -96,7 +115,9 @@ const AdminContextProvider = (props) => {
         appointments,
         setAppointments,
         getAllAppointments,
-        cancelAppointment
+        cancelAppointment,
+        getDashData,
+        dashData
     }
 
     return (
