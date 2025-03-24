9c09ea623bdc9d61a4a1d8746b50447da8ff9a28
nirdosh2004 <hrishabhkushwaha73@gmail.com>
2025-03-24 12:08:48 +0530

    13:44:24

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -3,6 +3,7 @@ import { assets } from '../assets/assets'
 import { AdminContext } from '../context/AdminContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { DoctorContext } from '../context/DoctorContext'
 
 const Login = () => {
 
@@ -12,23 +13,36 @@ const Login = () => {
 
 
     const { setAToken, backendUrl } = useContext(AdminContext)
+    const {setDToken} = useContext(DoctorContext)
 
     const onSubmitHandler = async (event) => {
         event.preventDefault()
         try {
-            if(state === 'Admin'){
-                const {data} = await axios.post(backendUrl + '/api/admin/login',{email,password})
-                if(data.success){
-                    localStorage.setItem('aToken',data.token)
+            if (state === 'Admin') {
+                const { data } = await axios.post(backendUrl + '/api/admin/login', { email, password })
+                if (data.success) {
+                    localStorage.setItem('aToken', data.token)
                     setAToken(data.token)
-                } else{
+                } else {
                     toast.error(data.message)
                 }
             } else {
 
+                const {data} = await axios.post(backendUrl + '/api/doctor/login', {email, password})
+
+                if (data.success) {
+                    localStorage.setItem('dToken', data.token)
+                    setDToken(data.token)
+                    console.log(data.token);
+                    
+                } else {
+                    toast.error(data.message)
+                }
+
+
             }
         } catch (error) {
-            
+
         }
     }
 
