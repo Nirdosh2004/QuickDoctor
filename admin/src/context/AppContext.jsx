cb507acc920ceb6adf1c6d20ecf0555875d58992
Nirdosh Kushwaha <nirdoshkushwaha75@gmail.com>
2025-03-30 20:03:15 +0530

    Everything done.

--- admin/src/context/AppContext.jsx
+++ admin/src/context/AppContext.jsx
@@ -5,13 +5,13 @@ export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
 
-    const currency = '$'
+    const currency = '₹'
 
     const calculateAge = (dob) => {
         const today = new Date()
         const birthDate = new Date(dob)
 
-        let age = today.getFullYear()-birthDate.getFullYear()
+        let age = today.getFullYear() - birthDate.getFullYear()
         return age
 
     }
@@ -19,10 +19,10 @@ const AppContextProvider = (props) => {
     const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 
     const slotDateFormat = (slotDate) => {
-      const dateArray = slotDate.split('/')
-      return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+        const dateArray = slotDate.split('/')
+        return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
     }
-  
+
 
     const value = {
         calculateAge,
